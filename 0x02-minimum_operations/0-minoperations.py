7df70992768b457b0c23684335da974b89832526
thattechgirlie <wanji.njoro1@gmail.com>
2024-09-15 12:23:45 +0300

    correction

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,19 +7,19 @@ def minOperations(n):
     text editor can execute only two operations
     in this file Copy All and Paste
     """
-   
+
     if (n < 2):
         return 0
     Operation, polynomial = 0, 2
     while polynomial <= n:
         # if n evenly divides by root
         if n % polynomial == 0:
-           
+
             Operation += polynomial
-            
+
             n = n / polynomial
-            
+
             polynomial -= 1
-  
+
         polynomial += 1
     return Operation
