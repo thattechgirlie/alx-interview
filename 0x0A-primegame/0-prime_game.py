c83ad79cfc89573b3c91c7cb11e25fcbbc1a5381
thattechgirlie <wanji.njoro1@gmail.com>
2024-10-06 12:49:44 +0300

    the game of prime

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" 0-prime_game.py"""
+
+
+def findMultiples(num, targets):
+    """ findMultiples within a file  """
+    for i in targets:
+        if i % num == 0:
+            targets.remove(i)
+    return targets
+
+
+def isPrime(i):
+    """ function to check if no is a prime no """
+    if i == 1:
+        return False
+    for j in range(2, i):
+        if i % j == 0:
+            return False
+    return True
+
+
+def findPrimes(n):
+    """ find prime no """
+    counter = 0
+    target = list(n)
+    for i in range(1, len(target) + 1):
+        if isPrime(i):
+            counter += 1
+            target.remove(i)
+            target = findMultiples(i, target)
+        else:
+            pass
+    return counter
+
+
+def isWinner(x, nums):
+    """
+   function to determine winner between Maria /Ben in a game of
+   Prime Numbers
+    """
+    players = {'Maria': 0, 'Ben': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
+                break
+        temp = findPrimes(cluster)
+
+        if temp % 2 == 0:
+            players['Ben'] += 1
+        elif temp % 2 != 0:
+            players['Maria'] += 1
+
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
+    elif players['Maria'] < players['Ben']:
+        return 'Ben'
+    else:
+        return None
